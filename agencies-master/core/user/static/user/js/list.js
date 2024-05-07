8b2f2137441c5a182e4be7394c0347b1ae92229c
bryanguevara <bryanguevara38@gmail.com>
2024-05-07 10:37:49 -0600

    SE HA QUITADO EL APARTADO DE CREAR Y ELIMINAR UN USUARIO, SOLO DE LA PARTE DE admin, SE ELIMINO EL CHECK DE SI ES STAFF PARA PODER ENTRAR AL ADMIN

--- agencies-master/core/user/static/user/js/list.js
+++ agencies-master/core/user/static/user/js/list.js
@@ -61,9 +61,9 @@ var user = {
             class: 'text-center',
             orderable: false,
             render: function (data, type, row) {
-                var buttons = '<a href="' + pathname + 'update/' + row[0] + '/" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
-                buttons += '<a rel="delete" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
-                return buttons;
+                return '<a href="' + pathname + 'update/' + row[0] + '/" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
+                // buttons += '<a rel="delete" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                // return buttons;
             }
         },
     ],
