ea30bfb9402866148336a76ea0744b96fadcfe0c
bryanguevara <bryanguevara38@gmail.com>
2024-05-12 22:10:13 -0600

    SE CORRIGIO ERROR DE VISUALIZACION DE LOS FORMULARIOS

--- agencies-master/core/pos/templatetags/pos_extras.py
+++ agencies-master/core/pos/templatetags/pos_extras.py
@@ -9,7 +9,11 @@ def is_checkbox(field):
     return field.field.widget.__class__.__name__ == CheckboxInput().__class__.__name__
 
 @register.filter()
-def split_form_field(form, quantity):
+def split_form_field(form, request):
+    if request.is_mobile:
+        quantity = 1
+    elif request.is_pc:
+        quantity = 3
     list_of_fields = form.visible_fields()
     splitted_list = []
 
