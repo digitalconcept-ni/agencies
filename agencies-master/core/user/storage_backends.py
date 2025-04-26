2d62d3c0ed5cefbf6152206e4ba833d2d713fc51
bryanguevara <bryanguevara38@gmail.com>
2025-04-25 19:55:18 -0600

    ADD: SE AGREGA ARCHIVO DE storage_backends.py

--- agencies-master/core/user/storage_backends.py
+++ agencies-master/core/user/storage_backends.py
@@ -1,44 +1,23 @@
-import os
+import datetime
 
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-from django_tenants.utils import get_tenant
-
+import os
 
 class TenantS3Storage(S3Boto3Storage):
-    def __init__(self, *args, **kwargs):
-        request = kwargs.pop('request', None)
-        tenant = get_tenant(request=request) if request else None
-        if tenant and tenant.schema_name != 'public':
-            self.location = f'media/{tenant.schema_name}'
-        else:
-            self.location = settings.AWS_LOCATION
+    def __init__(self, tenant_prefix=None, *args, **kwargs):
+        self.tenant_prefix = tenant_prefix if tenant_prefix and tenant_prefix != 'public' else ''
+        self.relative_root = getattr(settings, 'MULTITENANT_RELATIVE_MEDIA_ROOT', '')
         super().__init__(*args, **kwargs)
+
+    def get_location(self):
+        month = datetime.datetime.now().strftime("%m")
+        return os.path.join(settings.AWS_LOCATION, self.tenant_prefix, month, self.relative_root).lstrip('/')
 
     def _save(self, name, content):
         self.location = self.get_location()
-        # Extract the filename from the name argument
-        # filename = name.split('/')[-1]
-        # # Construct the full path based on the location and filename
-        # full_path = os.path.join(self.location, filename)
         return super()._save(name, content)
-
-    def get_location(self):
-        request = self._request()
-        print('request', request)
-        tenant = get_tenant(request=request) if request else None
-        print('tenant', tenant)
-        if tenant and tenant.schema_name != 'public':
-            return f'media/{tenant.schema_name}'
-        else:
-            return settings.AWS_LOCATION
 
     def url(self, name, query_auth=True, expire=None):
         self.location = self.get_location()
         return super().url(name, query_auth, expire)
-
-    def _request(self):
-        from django.utils.deprecation import MiddlewareMixin
-        if hasattr(MiddlewareMixin, '_thread_local'):
-            return getattr(MiddlewareMixin._thread_local, 'request', None)
-        return None
